9988751613a19a5eb2f878d56c022726d81b06e2
mr-loop-1 <loopolecreator1@gmail.com>
2023-07-03 18:09:43 +0530

    FEAT: added all mdx conversion code

---
+++ data/headerNavLinks.js
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/about', title: 'About' },
+  { href: './books', title: 'Books' }
+];
+
+export default headerNavLinks;
